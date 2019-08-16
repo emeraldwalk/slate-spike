c394e04a17ab26543c1e23edb380ea6ca7bc87c1
Brian Ingles <github@emeraldwalk.com>
2019-08-15 23:50:33 -0500

    Small refactor

--- src/plugins/word.tsx
+++ src/plugins/word.tsx
@@ -3,7 +3,7 @@ import { Plugin, Range, Value } from 'slate';
 export default function WordPlugin(): Plugin {
   return {
     onChange: (editor, next) => {
-      const word = findWord(editor.value);
+      const word = findWordAtFocus(editor.value);
       console.log(word);
       if(word) {
         console.log(word.text);
@@ -18,17 +18,10 @@ interface TextRange extends Range {
   text?: string;
 }
 
-function findWord(
-  value: Value
-): TextRange | null {
-  if(!value.focusText) {
-    return null;
-  }
-
-  const text = value.focusText.text;
-  const focus = value.selection.focus;
-  const offset = focus.offset;
-
+function findWordAtOffset(
+  text: string,
+  offset: number
+) {
   const start = text.lastIndexOf(
     ' ',
     offset - 1
@@ -43,6 +36,29 @@ function findWord(
     end = text.length;
   }
 
+  return {
+    start,
+    end,
+    text: text.substring(start, end)
+  };
+}
+
+function findWordAtFocus(
+  value: Value
+): TextRange | null {
+  if(!value.focusText) {
+    return null;
+  }
+
+  const fullText = value.focusText.text;
+  const focus = value.selection.focus;
+  const offset = focus.offset;
+
+  const { start, end, text } = findWordAtOffset(
+    fullText,
+    offset
+  );
+
   const path = focus.path.toArray();
 
   const range: TextRange = Range.create({
@@ -56,10 +72,7 @@ function findWord(
     }
   });
 
-  range.text = text.substring(
-    start,
-    end
-  );
+  range.text = text;
 
   return range;
 }
